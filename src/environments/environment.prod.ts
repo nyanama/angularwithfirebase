9f54a8efbccd98443f6a051a2767bd10f0b14c53
Nagendra <nyanamadala@gmail.com>
2018-07-17 02:26:12 +0530

    changes commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  fireBase:{
+    apiKey: "AIzaSyA5sOcI8qwwIdiuMEPK2GMlzwdQo8Yr8sk",
+    authDomain: "learnangular-d1c09.firebaseapp.com",
+    databaseURL: "https://learnangular-d1c09.firebaseio.com",
+    projectId: "learnangular-d1c09",
+    storageBucket: "learnangular-d1c09.appspot.com",
+    messagingSenderId: "345718390782"
+  },
+  PORT:8080
 };
